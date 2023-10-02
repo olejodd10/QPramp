ba0f97740c2a12ac4dcc38b0acbcc41e878b9405
Ole Johan <olejodd10@gmail.com>
2023-10-02 13:54:06 +0200

    Implemented solve_qp function that solves (4).

--- algs.c
+++ algs.c
@@ -95,6 +95,23 @@ static void compute_u(size_t m, size_t n, size_t c, const double neg_invh_f[m][n
     }
 }
 
+static void compute_z(size_t c, size_t p, const double neg_g_invh[c][p], const double y[c], double z[p]) {
+    memset(z, 0, sizeof(double)*p);
+    for (size_t i = 0; i < c; ++i) {
+        if (y[i] > EPS) {
+            add_scaled_vector(p, z, neg_g_invh[i], y[i], z);
+        }
+    }
+}
+
+void solve_qp(size_t c, size_t n, size_t p, const double neg_g_invh_gt[c][c], const double neg_s[c][n], const double neg_w[c], const double neg_g_invh[c][p], const double x[n], double invq[c][c], iterable_set_t* a_set, double y[c], double v[c], double z[p]) {
+    set_clear(a_set);
+    matrix_vector_product(c, n, neg_s, x, y);
+    vector_sum(c, y, neg_w, y);
+    algorithm1(c, invq, a_set, neg_g_invh_gt, v, y);
+    compute_z(c, p, neg_g_invh, y, z);
+}
+
 // This only represents one iteration of algorithm2, i.e. algorithm2 without the lines with "while"
 // Typically you will have an MPC loop where a measured/estimated x is given as an input and then u is computed and applied
 // Note that y is modified
--- algs.h
+++ algs.h
@@ -8,6 +8,8 @@
 #include "vector.h"
 #include "iterable_set.h"
 
+void solve_qp(size_t c, size_t n, size_t p, const double neg_g_invh_gt[c][c], const double neg_s[c][n], const double neg_w[c], const double neg_g_invh[c][p], const double x[n], double invq[c][c], iterable_set_t* a_set, double y[c], double v[c], double z[p]);
+
 void algorithm2(size_t c, size_t n, size_t m, const double neg_g_invh_gt[c][c], const double neg_s[c][n], const double neg_w[c], const double neg_invh_f[m][n], const double neg_g_invh[c][m], const double x[n], double invq[c][c], iterable_set_t* a_set, double y[c], double v[c], double u[m]);
 
 #endif
