4161c636f85128a513abc9b19e86045a455d0bec
Ole Johan <olejodd10@gmail.com>
2023-12-04 19:07:41 +0100

    Added void in empty parameter lists.

--- include/timing.h
+++ include/timing.h
@@ -1,10 +1,10 @@
 #ifndef TIMING_H
 #define TIMING_H
 
-void timing_print_precision();
+void timing_print_precision(void);
 
-void timing_reset();
+void timing_reset(void);
 
-long timing_elapsed();
+long timing_elapsed(void);
 
 #endif
--- src/timing.c
+++ src/timing.c
@@ -7,16 +7,16 @@
 
 static struct timespec then, now;
 
-void timing_print_precision() {
+void timing_print_precision(void) {
     clock_getres(TIMING_CLOCK, &then);
     printf("Timing precision is %ld s %ld ns\n", then.tv_sec, then.tv_nsec);
 }
 
-void timing_reset() {
+void timing_reset(void) {
     clock_gettime(TIMING_CLOCK, &then);
 }
 
-long timing_elapsed() {
+long timing_elapsed(void) {
     clock_gettime(TIMING_CLOCK, &now);
     return (now.tv_sec-then.tv_sec)*1000000000 + now.tv_nsec-then.tv_nsec;
 }
