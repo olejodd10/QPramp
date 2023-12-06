3591ed8f8aeabbb712e9ba3e2234e920f7ed05d3
Ole Johan <olejodd10@gmail.com>
2023-12-07 00:04:48 +0100

    Made indexed_vectors more robust.

--- src/indexed_vectors.c
+++ src/indexed_vectors.c
@@ -6,7 +6,10 @@
 
 void indexed_vectors_clear(indexed_vectors_t *vecs) {
     for (size_t i = 0; i < vecs->capacity; ++i) {
-        vecs->whats_here[i] = vecs->max_index; 
+        if (vecs->whats_here[i] != vecs->max_index) {
+            vecs->where_is[vecs->whats_here[i]] = vecs->capacity;
+            vecs->whats_here[i] = vecs->max_index; 
+        }
     }
     vecs->size = 0;
 }
@@ -18,7 +21,13 @@ void indexed_vectors_init(indexed_vectors_t *vecs, size_t capacity, size_t lengt
 	vecs->where_is = (size_t*)malloc(max_index*sizeof(size_t)); 
 	vecs->whats_here = (size_t*)malloc(capacity*sizeof(size_t)); 
     vecs->values = (double*)malloc(capacity*length*sizeof(size_t));
-    indexed_vectors_clear(vecs);
+    for (size_t i = 0; i < vecs->max_index; ++i) {
+        vecs->where_is[i] = vecs->capacity; 
+    }
+    for (size_t i = 0; i < vecs->capacity; ++i) {
+        vecs->whats_here[i] = vecs->max_index; 
+    }
+    vecs->size = 0;
 }
 
 void indexed_vectors_destroy(indexed_vectors_t *vecs) {
@@ -27,7 +36,7 @@ void indexed_vectors_destroy(indexed_vectors_t *vecs) {
     free(vecs->values);
 }
 
-static size_t first_free(const indexed_vectors_t *vecs) {
+static size_t first_available_slot(const indexed_vectors_t *vecs) {
     for (size_t i = 0; i < vecs->capacity; ++i) {
         if (vecs->whats_here[i] == vecs->max_index) {
             return i;
@@ -37,15 +46,27 @@ static size_t first_free(const indexed_vectors_t *vecs) {
 }
 
 double* indexed_vectors_get_mut(indexed_vectors_t *vecs, size_t index) {
-    return &vecs->values[vecs->length*vecs->where_is[index]];
+    if (index >= vecs->max_index || vecs->where_is[index] == vecs->capacity) {
+        return NULL;
+    } else {
+        return &vecs->values[vecs->length*vecs->where_is[index]];
+    }
 }
 
 const double* indexed_vectors_get(const indexed_vectors_t *vecs, size_t index) {
-    return &vecs->values[vecs->length*vecs->where_is[index]];
+    if (index >= vecs->max_index || vecs->where_is[index] == vecs->capacity) {
+        return NULL;
+    } else {
+        return &vecs->values[vecs->length*vecs->where_is[index]];
+    }
 }
 
 void indexed_vectors_insert(indexed_vectors_t *vecs, size_t index, const double *vec) {
-    size_t target = first_free(vecs);
+    if (index >= vecs->max_index || vecs->where_is[index] != vecs->capacity 
+            || vecs->size == vecs->capacity) {
+        return;
+    }
+    size_t target = first_available_slot(vecs);
     vecs->whats_here[target] = index;
     vecs->where_is[index] = target;
     memcpy(&vecs->values[vecs->length*target], vec, vecs->length*sizeof(double));
@@ -53,6 +74,9 @@ void indexed_vectors_insert(indexed_vectors_t *vecs, size_t index, const double 
 }
 
 void indexed_vectors_remove(indexed_vectors_t *vecs, size_t index) {
+    if (index >= vecs->max_index || vecs->where_is[index] == vecs->capacity) {
+        return;
+    }
     vecs->whats_here[vecs->where_is[index]] = vecs->max_index;
     vecs->where_is[index] = vecs->capacity;
     vecs->size--;
