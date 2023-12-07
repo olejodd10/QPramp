104ec6b8e30db3c52e37092dabd7fbdf61971e78
Ole Johan <olejodd10@gmail.com>
2023-12-07 01:25:10 +0100

    Documented vector.h and removed some vector.c comments.

--- include/vector.h
+++ include/vector.h
@@ -3,19 +3,58 @@
 
 #include <stddef.h>
 
+/**
+ * @brief Compute inner product of two vectors.
+ *
+ * @param[in] n Vector length
+ * @param[in] v1 Array with length n representing a vector
+ * @param[in] v2 Array with length n representing a vector
+ * @return The inner product of v1 and v2
+ */
 double vector_inner_product(size_t n, const double v1[n], const double v2[n]);
 
-// https://stackoverflow.com/questions/3412190/net-decimal-negate-vs-multiplying-by-1
-// vec == res er lov
+/**
+ * @brief Negate a vector.
+ *
+ * @param[in] n Vector length
+ * @param[in] vec Array with length n representing a vector
+ * @param[out] res Array with length n for storing result -vec
+ * @note vec and res can be the same arrays
+ */
 void vector_negate(size_t n, const double vec[n], double res[n]);
 
-// vec == res er lov
+/**
+ * @brief Scale a vector.
+ *
+ * @param[in] n Vector length
+ * @param[in] vec Array with length n representing a vector
+ * @param[in] c Factor to scale vec by
+ * @param[out] res Array with length n for storing result c*vec
+ * @note vec and res can be the same arrays
+ */
 void vector_scale(size_t n, const double vec[n], double c, double res[n]);
 
-// res == v1 og res == v2 er lov
+/**
+ * @brief Add two vectors.
+ *
+ * @param[in] n Vector length
+ * @param[in] v1 Array with length n representing a vector
+ * @param[in] v2 Array with length n representing a vector
+ * @param[out] res Array with length n for storing result v1+v2
+ * @note v1, v2 and res can be the same arrays
+ */
 void vector_add(size_t n, const double v1[n], const double v2[n], double res[n]);
 
-// res == v1 og res == v2 er lov
+/**
+ * @brief Add a vector and a scaled vector.
+ *
+ * @param[in] n Vector length
+ * @param[in] v1 Array with length n representing a vector
+ * @param[in] v2 Array with length n representing a vector
+ * @param[in] c Factor to scale v2 by
+ * @param[out] res Array with length n for storing result v1+c*v2
+ * @note v1, v2 and res can be the same arrays
+ */
 void vector_add_scaled(size_t n, const double v1[n], const double v2[n], double c, double res[n]);
 
 #endif
--- src/vector.c
+++ src/vector.c
@@ -10,29 +10,24 @@ double vector_inner_product(size_t n, const double v1[n], const double v2[n]) {
     return result;
 }
 
-// https://stackoverflow.com/questions/3412190/net-decimal-negate-vs-multiplying-by-1
-// vec == res er lov
 void vector_negate(size_t n, const double vec[n], double res[n]) {
     for (size_t i = 0; i < n; ++i) {
         res[i] = -vec[i];
     }
 }
 
-// vec == res er lov
 void vector_scale(size_t n, const double vec[n], double c, double res[n]) {
     for (size_t i = 0; i < n; ++i) {
         res[i] = c*vec[i];
     }
 }
 
-// res == v1 og res == v2 er lov
 void vector_add(size_t n, const double v1[n], const double v2[n], double res[n]) {
     for (size_t i = 0; i < n; ++i) {
         res[i] = v1[i] + v2[i];
     }
 }
 
-// res == v1 og res == v2 er lov
 void vector_add_scaled(size_t n, const double v1[n], const double v2[n], double c, double res[n]) {
     for (size_t i = 0; i < n; ++i) {
         res[i] = v1[i] + c*v2[i];
