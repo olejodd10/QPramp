7fab768ed76c5a7a66e789e8792073919ad1c86a
Ole Johan <olejodd10@gmail.com>
2023-12-10 19:29:27 +0100

    Documented csv.h.

--- include/csv.h
+++ include/csv.h
@@ -4,20 +4,68 @@
 #include <stddef.h>
 #include <sys/types.h>
 
+/**
+ * @brief Parse the height of a matrix stored as CSV
+ *
+ * @param[in] path Path to CSV file
+ * @return Matrix height on success,
+ *         <0 on error
+ */
 ssize_t csv_parse_matrix_height(const char* path); 
 
+/**
+ * @brief Parse the width of a matrix stored as CSV
+ *
+ * @param[in] path Path to CSV file
+ * @return Matrix width on success,
+ *         <0 on error
+ */
 ssize_t csv_parse_matrix_width(const char* path); 
 
-// Returns error if file size is incompatible or if any reads fail
-// Returns error if row vector is given, although the implementation would handle it
+/**
+ * @brief Parse a vector stored as CSV
+ *
+ * @param[in] path Path to CSV file
+ * @param[in] n Length of vector
+ * @param[out] res Array with length n for storing vector
+ * @return 0 on success,
+ *         <0 on error
+ */
 int csv_parse_vector(const char* path, size_t n, double res[n]); 
 
-// Returns error if file size is incompatible or if any reads fail
+/**
+ * @brief Parse a matrix stored as CSV
+ *
+ * @param[in] path Path to CSV file
+ * @param[in] m Height of matrix
+ * @param[in] n Width of matrix
+ * @param[out] res Flattened m by n array for storing matrix
+ * @return 0 on success,
+ *         <0 on error
+ */
 int csv_parse_matrix(const char* path, size_t m, size_t n, double res[m][n]); 
 
-// Writes as a column vector
+/**
+ * @brief Store a vector as a CSV column
+ *
+ * @param[in] path Path for CSV file
+ * @param[in] n Length of vector
+ * @param[in] vec Array with length n to store
+ * @return Number of bytes written if successful,
+ *         <0 on error
+ */
 ssize_t csv_save_vector(const char* path, size_t n, const double vec[n]); 
 
+/**
+ * @brief Store a matrix as CSV
+ *
+ * @param[in] path Path for CSV file
+ * @param[in] m Matrix height
+ * @param[in] n Matrix width
+ * @param[in] mat Flattened m by n array to store
+ * @return Number of bytes written if successful,
+ *         <0 on error
+ */
 ssize_t csv_save_matrix(const char* path, size_t m, size_t n, const double mat[m][n]); 
 
 #endif
