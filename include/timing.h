2b78d5151b2749ab173fd30c016ffbc54f4cb5d4
Ole Johan <olejodd10@gmail.com>
2023-12-10 19:01:57 +0100

    Documented timing.h.

--- include/timing.h
+++ include/timing.h
@@ -1,10 +1,23 @@
 #ifndef TIMING_H
 #define TIMING_H
 
+/**
+ * @brief Print the precision of the timer.
+ *
+ */
 void timing_print_precision(void);
 
+/**
+ * @brief Reset the timer.
+ *
+ */
 void timing_reset(void);
 
+/**
+ * @brief Get the number of nanoseconds since the timer was last reset.
+ *
+ * @return The number of nanoseconds since the timer was last reset
+ */
 long timing_elapsed(void);
 
 #endif
