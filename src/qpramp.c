ab802624fb881a460cf3c6f5fa3f3b5a48287676
Ole Johan <olejodd10@gmail.com>
2023-12-16 18:51:50 +0100

    Removed unnecessary comments in qpramp.c.

--- src/qpramp.c
+++ src/qpramp.c
@@ -95,13 +95,12 @@ static int compute_v(size_t c, const indexed_vectors_t *invq, const iterable_set
 
 // Returns c if none found
 static inline size_t rank_2_update_removal_index(size_t c, size_t i, const indexed_vectors_t *invq, const iterable_set_t* a_set, const double neg_g_invh_gt[c][c], const double y[c]) {
-    double max = 0.0; // Overwritten immediately
+    double max = 0.0;
     size_t index = c;
     for (size_t j = set_first(a_set); j != set_end(a_set); j = set_next(a_set, j)) {
         double divisor = 0.0;
         for (size_t k = set_first(a_set); k != set_end(a_set); k = set_next(a_set, k)) {
-            // Note that since j is in a_set, we only have to consider "active columns" of invq
-            // Also note that the order of indices for neg_g_invh_gt doesn't matter since it's symmetric
+            // Note that the order of indices for neg_g_invh_gt doesn't matter since it's symmetric
             divisor += indexed_vectors_get(invq, k)[j] * neg_g_invh_gt[i][k];
         }
         if ((divisor < -QPRAMP_EPS) && (y[j]/divisor > max || index == c)) {
@@ -123,8 +122,6 @@ static inline void update_y(size_t c, size_t index, const double v[c], double y[
     vector_add_scaled(c, y, v, y[index], y);
 }
 
-// Terminal results are written to and available from a_set, invq and y
-// v can be anything - call it temp?
 static int algorithm1(size_t c, size_t p, indexed_vectors_t *invq, iterable_set_t* a_set, const double neg_g_invh_gt[c][c], double v[c], double y[c]) {
     while (1) {
         size_t index = most_negative_index(c, a_set, y);
@@ -166,7 +163,7 @@ static int algorithm1(size_t c, size_t p, indexed_vectors_t *invq, iterable_set_
             update_y(c, index, v, y);
             update_invq(c, index, a_set, v, invq);
             set_insert(a_set, index);
-            indexed_vectors_insert(invq, index, v); // Note that the value of v was intentionally changed in update_y_and_invq
+            indexed_vectors_insert(invq, index, v);
             indexed_vectors_get_mut(invq, index)[index] += 1.0; // Pretend there was a unit vector in the column to start with
         }
     }
@@ -211,9 +208,7 @@ int qpramp_solve(size_t c, size_t n, size_t p, const double neg_g_invh_gt[c][c],
     return 0;
 }
 
-// This only represents one iteration of algorithm2, i.e. algorithm2 without the lines with "while"
 // Typically you will have an MPC loop where a measured/estimated x is given as an input and then u is computed and applied
-// Note that y is modified
 int qpramp_solve_mpc(size_t c, size_t n, size_t m, size_t p, const double neg_g_invh_gt[c][c], const double neg_s[c][n], const double neg_w[c], const double neg_invh_f[m][n], const double neg_g_invh[c][m], const double x[n], double u[m]) {
     int err = solve_y(c, n, p, &invq, &a_set, neg_g_invh_gt, neg_s, neg_w, x, v, y);
     if (err) {
